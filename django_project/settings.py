36ce71245dee3e20a6573f313b6c320b2775ae65
Ann 杏 Kilzer <akilzer@gmail.com>
2025-07-06 23:17:24 +0900

    newspaper app

--- django_project/settings.py
+++ django_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     # local
     "accounts",
+    "articles",
     "pages",
 ]
 
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Tokyo"
 
 USE_I18N = True
 
--- django_project/urls.py
+++ django_project/urls.py
@@ -21,7 +21,8 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),  # new
-    path("accounts/", include("django.contrib.auth.urls")),  # new
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("articles/", include("articles.urls")),
     path("", include("pages.urls")),
 ]
